ef33c706aa47aa3d2bc752b7682dde4ffdfe1cd9
G THARUN KUMAR <120480184+BULLET9557@users.noreply.github.com>
2024-01-16 12:07:04 +0530

    Update UsersController.java

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
 
+import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
@@ -17,6 +21,9 @@ public class UsersController {
 	
 	@Autowired
 	UsersService service;
+	
+	@Autowired
+	SongService songService;
 	
 	@PostMapping("/register")
 	public String addUsers(@ModelAttribute Users user)
@@ -38,7 +45,7 @@ public class UsersController {
 	@PostMapping("/validate")
 	public String validate(@RequestParam("email") String email,
 			@RequestParam("password") String password,
-			HttpSession session) {
+			HttpSession session , Model model) {
 		
 		if(service.validateUser(email,password)==true)
 		{
@@ -54,6 +61,9 @@ public class UsersController {
 			{
 				Users user=service.getUser(email);
 				boolean userStatus=user.isPremium();
+				List<Song> songList=songService.fetchAllSongs();
+				model.addAttribute("songs",songList);
+				
 				model.addAttribute("isPremium",userStatus);
 				return "customerHome";
 			}
@@ -65,7 +75,18 @@ public class UsersController {
 				
 	}
 	
+	/*
+	 * @GetMapping("/pay") public String pay(@RequestParam String email) { boolean
+	 * paymentStatus=false; //payment API if(paymentStatus == true) { Users
+	 * user=service.getUser(email); user.setPremium(true); service.updateUser(user);
+	 * 
+	 * } return "login"; }
+	 */
 	
+	/*
+	 * @GetMapping("/logout") public String logout(HttpSession session) {
+	 * session.invalidate(); return "logout"; }
+	 */
 	
 
 }
